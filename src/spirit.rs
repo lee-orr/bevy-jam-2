0ba606e23226fc9e4f6be8340b3c6d4fce94ec2e
lee-orr <lee-orr@users.noreply.github.com>
2022-08-20 22:52:33 -0400

    better movement for spirits

--- src/spirit.rs
+++ src/spirit.rs
@@ -20,7 +20,7 @@ impl Plugin for SpiritPlugin {
 }
 
 #[derive(Component)]
-pub struct Spirit;
+pub struct Spirit(Vec3);
 
 fn spawn_spirit(
     mut commands: Commands,
@@ -37,7 +37,7 @@ fn spawn_spirit(
             material: materials.add(ColorMaterial::from(Color::BLUE)),
             ..default()
         })
-        .insert(Spirit)
+        .insert(Spirit(Vec3::ZERO))
         .insert(AudioEmitter(assets.bass_1.clone(), "Bass".to_owned()));
     commands
         .spawn_bundle(MaterialMesh2dBundle {
@@ -48,7 +48,7 @@ fn spawn_spirit(
             material: materials.add(ColorMaterial::from(Color::BLUE)),
             ..default()
         })
-        .insert(Spirit);
+        .insert(Spirit(Vec3::ZERO));
     commands
         .spawn_bundle(MaterialMesh2dBundle {
             mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
@@ -58,7 +58,7 @@ fn spawn_spirit(
             material: materials.add(ColorMaterial::from(Color::BLUE)),
             ..default()
         })
-        .insert(Spirit);
+        .insert(Spirit(Vec3::ZERO));
     commands
         .spawn_bundle(MaterialMesh2dBundle {
             mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
@@ -68,13 +68,13 @@ fn spawn_spirit(
             material: materials.add(ColorMaterial::from(Color::BLUE)),
             ..default()
         })
-        .insert(Spirit);
+        .insert(Spirit(Vec3::ZERO));
 }
 
 fn spirit_random_walk(
     mut spirits: Query<
-        &mut Transform,
-        (With<Spirit>, Without<Collecting>, Without<PlayerControl>),
+        (&mut Transform, &mut Spirit),
+        (Without<Collecting>, Without<PlayerControl>),
     >,
     players: Query<&Transform, With<PlayerControl>>,
     time: Res<Time>,
@@ -95,15 +95,23 @@ fn spirit_random_walk(
         Vec3::ZERO
     };
 
-    for mut spirit in spirits.iter_mut() {
+    for (mut spirit, mut velocity) in spirits.iter_mut() {
         let direction = spirit.translation - player_position;
         let direction = direction.normalize();
 
-        spirit.translation += direction * delta * speed;
+        velocity.0 += direction * delta * delta * speed;
 
-        spirit.translation.x =
-            spirit.translation.x.clamp(-1. * bounds.0, bounds.0);
-        spirit.translation.y =
-            spirit.translation.y.clamp(-1. * bounds.1, bounds.1);
+        if velocity.0.length() > 100. {
+            velocity.0 = velocity.0.normalize() * 99.;
+        }
+
+        spirit.translation += velocity.0 * delta * speed;
+
+        if spirit.translation.x.abs() > bounds.0 {
+            velocity.0.x = -1. * velocity.0.x;
+        }
+        if spirit.translation.y.abs() > bounds.1 {
+            velocity.0.y = -1. * velocity.0.y;
+        }
     }
 }
